ecf56ed0ffdf3841d701c43f422345b434896e98
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-05-02 12:51:41 -0700

    created disabled button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,13 +4,32 @@ import { lighten, darken, readableColor } from 'polished';
 import { Props } from './index';
 
 export const Container = styled.button<Props>`
-  background-color: ${({ backgroundColor, outlined }: Props) => outlined ? 'transparent' : backgroundColor} !important;
-  color: ${({ color }: Props) => readableColor(color as string, '#fff', '#011030')};
-
+  background-color: ${(props) =>
+    props.outlined ? 'transparent' : props.backgroundColor} !important;
+  color: ${({ color }: Props) =>
+    readableColor(color as string, '#fff', '#19181f')} !important;
   padding: 18px 45px;
   text-transform: uppercase;
   font-weight: bold;
-
   border-radius: 4px;
-  border: ${({ backgroundColor, outlined }: Props) => outlined ? `2px solid ${backgroundColor}` : 'none'};
+  border: ${(props) =>
+    props.outlined ? `2px solid ${props.backgroundColor}` : 'none'};
+  cursor: pointer;
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+  &:hover:not(:disabled) {
+    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
+    border-color: transparent;
+    background-color: ${(props) =>
+      props.outlined
+        ? props.backgroundColor
+        : lighten(0.03, props.backgroundColor as string)};
+  }
+  :active:not(:disabled) {
+    box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
+    background-color: ${(props) =>
+      darken(0.03, props.backgroundColor as string)};
+  }
 `;
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -6,4 +6,4 @@ import { Button } from '../src';
 storiesOf('Button', module)
   .add('Default', () => <Button>Default</Button>)
   .add('Outlined', () => <Button outlined>Outlined</Button>)
-  // .add('Disabled', () => <Button disabled>Default</Button>);
+  .add('Disabled', () => <Button disabled>Default</Button>);
